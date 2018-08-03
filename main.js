e9f4d3bf0bb0e940ffc9cb26c5b1ce7890a28504
Alan Fajayan <alan.fajayan@gmail.com>
2018-08-03 12:38:59 -0400

    Completed course

--- main.js
+++ main.js
@@ -2,8 +2,18 @@ $(document).ready(function() {
     // on page load, collapse all panels
     $(".panel-body").hide();
     // retrieve and show partials inside divs
-    $.get("_module01.html",function(data){
-        $("#divM01-content").append(data);
+    loadPartials("_module01.html","divM01-content");
+    loadPartials("_module02.html","divM02-content");
+    loadPartials("_module03.html","divM03-content");
+    loadPartials("_module04.html","divM04-content");
+    loadPartials("_module05.html","divM05-content");
+    loadPartials("_module06.html","divM06-content");
+    loadPartials("_module07.html","divM07-content");
+    loadPartials("_module08.html","divM08-content");
+    var module09 = $.Deferred();
+    module09 = loadPartials("_module09.html","divM09-content");
+    $.when(module09).done(function(){
+        loadPartials("_tree.html","dir-tree");
     });
 
     $(document).on("click", "body *", function() {        
@@ -16,7 +26,33 @@ $(document).ready(function() {
                 break;  
             case "panel-heading-M02":
                 $("#panel-body-M02").slideToggle();
-                break;                      
+                break; 
+            case "panel-heading-M03":
+                $("#panel-body-M03").slideToggle();
+                break;  
+            case "panel-heading-M04":
+                $("#panel-body-M04").slideToggle();
+                break;
+            case "panel-heading-M05":
+                $("#panel-body-M05").slideToggle();
+                break;
+            case "panel-heading-M06":
+                $("#panel-body-M06").slideToggle();
+                break;
+            case "panel-heading-M07":
+                $("#panel-body-M07").slideToggle();
+                break;
+            case "panel-heading-M08":
+                $("#panel-body-M08").slideToggle();
+                break;
+            case "panel-heading-M09":
+                $("#panel-body-M09").slideToggle();
+                break;                    
         }									
-    });	    
+    });	 
+    function loadPartials(file,element) {
+        $.get(file,function(data){
+            $("#" + element).append(data);
+        });
+    }  
 });
